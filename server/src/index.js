19b9c965d75236542a6c4ee7d211bf9799ebe565
Franco Quevedo <francoq96@gmail.com>
2023-05-24 10:52:18 -0300

    Refactorizado de configuraciones del index.js

--- server/src/index.js
+++ server/src/index.js
@@ -1,26 +1,27 @@
-import * as dotenv from "dotenv";
+import * as dotenv from "dotenv"
 // Toma las variables configuradas por ENV dentro del docker-compose / dockerfile
-// dotenv.config({ path: './.env'});
-dotenv.config();
+// dotenv.config({ path: './.env'})
+dotenv.config()
 
-import expressServer from "express";
-import bodyParser from "body-parser";
-import morgan from "morgan";
-import cors from "cors";
-import Sequelize from "sequelize";
+import expressServer from "express"
+import bodyParser from "body-parser"
+import morgan from "morgan"
+import cors from "cors"
 
-import fisicaRouter from "./routes/fisica.routes.js";
+import { dbConnection } from "./db/dbconfig.js"
 
-const app = expressServer();
-const PORT = process.env.SERVER_PORT || 3000;
+import fisicaRouter from "./routes/fisica.routes.js"
+
+const app = expressServer()
+const PORT = process.env.SERVER_PORT || 3000
 
 //Necesitamos body-parser para formatear los post en express
-app.use(morgan("dev"));
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-app.use(cors());
+app.use(morgan("dev"))
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+app.use(cors())
 
-app.use("/api/fisica", fisicaRouter);
+app.use("/api/fisica", fisicaRouter)
 
 // app.use("/public-key", "id_rsa.pub") // formato x.509
 
@@ -28,21 +29,7 @@ app.use("/api/fisica", fisicaRouter);
 
 // Levantamos el servidor para que escuche peticiones
 app.listen(PORT, () => {
-  console.log(`Server on container port ${PORT}`);
-  console.log(`Server on localhost port ${process.env.LOCALHOST_PORT}`);
-  console.log("----------------------");
-});
+    console.log(`LabRem Fisica - Server on ${process.env.LOCALHOST_PORT}:${PORT}`)
+})
 
-export const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    dialect: process.env.DB_DIALECT
-  }
-);
-export function delay(time) {
-  return new Promise(resolve => setTimeout(resolve, time));
-} 
+export const db = dbConnection
