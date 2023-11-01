d7e41ea22cd2cc406216d14df8bdb4f03abd6421
Franco Quevedo <fquevedo@onebittech.com>
2023-11-01 15:41:01 -0300

    fix: actualizado control de arduino

--- server/src/utils/arduino.js
+++ server/src/utils/arduino.js
@@ -1,29 +1,27 @@
-import axios from "axios"
-import { delay } from "./delay.js"
+import { exec } from 'child_process'
 
 const URL_ARDUINO = process.env.URL_ARDUINO
 
 const GET  = 'GET /  HTTP/1.1'
 const POST = 'POST / HTTP/1.1'
 
-// curl -X GET http://10.0.255.110 -H 'Content-Type: text/plain' -d 'GET /  HTTP/1.1'
+// curl -X GET http://10.0.255.11x -H 'Content-Type: text/plain' -d 'GET /  HTTP/1.1'
 async function arduinoGET() {
     // const curlGET = `curl -X GET ${URL_ARDUINO} -H 'Content-Type: text/plain' -d '${GET} ${body}'`
-    const headers = {
-        'Content-Type': 'text/plain'
-    }
+    const curlGET = `curl -X GET ${URL_ARDUINO} -H 'Content-Type: text/plain' -d '${GET}'`
 
-    let response
-    try {
-        response = await axios.get(URL_ARDUINO, { headers, GET })
-        console.log(response.data)
-    } catch (error) {
-        console.error('Error:', error.message)
-    }
-    return response
+    return new Promise((resolve, reject) => {
+        exec(curlGET, (error, stdout, stderr) => {
+            if (error) {
+                reject(error)
+                return
+            }
+            resolve(stdout)
+        })
+    })
 }
 
-// curl -X POST http://10.0.255.110 -H 'Content-Type: text/plain' -d 'POST / HTTP/1.1 {"Estado": [3,true,true],"Analogico": ["central",40,40]}'
+// curl -X POST http://10.0.255.11x -H 'Content-Type: text/plain' -d 'POST / HTTP/1.1 {"Estado": [3,true,true],"Analogico": ["central",40,40]}'
 async function arduinoPOST(diafragma, distanciaLente, distanciaPantalla) {
     const body = `{"Estado": [3,true,true],"Analogico": [${diafragma},${distanciaLente},${distanciaPantalla}]}`
     const curlPOST = `curl -X POST ${URL_ARDUINO} -H 'Content-Type: text/plain' -d '${POST} ${body}'`
@@ -39,3 +37,4 @@ async function arduinoPOST(diafragma, distanciaLente, distanciaPantalla) {
     })
 }
 
+export { arduinoGET, arduinoPOST }
