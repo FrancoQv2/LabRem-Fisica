3b50d7f9b552dcbc87685d952c3aae0123ff4d60
Franco Quevedo <fquevedo@onebittech.com>
2023-11-01 15:35:28 -0300

    ci: update docker compose prod

--- server/src/utils/arduino.js
+++ server/src/utils/arduino.js
@@ -1,175 +1,41 @@
 import axios from "axios"
 import { delay } from "./delay.js"
 
-// const URL_ARDUINO = 'http://192.168.100.75:3031/api/control/arduino' // Cambiar por ip arduino
-const URL_ARDUINO = "http://172.23.12.125:5033" // IP de wsl vista desde dev-teleco
+const URL_ARDUINO = process.env.URL_ARDUINO
 
-export async function postArduino(diafragma, distanciaLente, distanciaPantalla) {
-    let respuestaPost
+const GET  = 'GET /  HTTP/1.1'
+const POST = 'POST / HTTP/1.1'
 
-    const body = {
-        // [ Laboratorio (Fisica), Sub Laboratorio (Convergente), Inicio del experimento ]
-        "Estado": [3, true, true],
-        "Analogico": [diafragma, distanciaLente, distanciaPantalla]
+// curl -X GET http://10.0.255.110 -H 'Content-Type: text/plain' -d 'GET /  HTTP/1.1'
+async function arduinoGET() {
+    // const curlGET = `curl -X GET ${URL_ARDUINO} -H 'Content-Type: text/plain' -d '${GET} ${body}'`
+    const headers = {
+        'Content-Type': 'text/plain'
     }
 
-    try {
-        respuestaPost = axios.post(`${URL_ARDUINO}/api/fisica`, body)
-    } catch (error) {
-        console.log(error);
-    }
-    
-    return respuestaPost
-}
-
-export async function getArduino() {
     let response
     try {
-        response = await axios.get(`${URL_ARDUINO}/api/fisica`)
+        response = await axios.get(URL_ARDUINO, { headers, GET })
+        console.log(response.data)
     } catch (error) {
-        console.log(error)
+        console.error('Error:', error.message)
     }
-
     return response
 }
 
-// async function postEnsayoConvergenteTEST (req, res) {
-//     console.log(`-\n--> postEnsayoConvergente - ${JSON.stringify(req.body)}\n---`)
+// curl -X POST http://10.0.255.110 -H 'Content-Type: text/plain' -d 'POST / HTTP/1.1 {"Estado": [3,true,true],"Analogico": ["central",40,40]}'
+async function arduinoPOST(diafragma, distanciaLente, distanciaPantalla) {
+    const body = `{"Estado": [3,true,true],"Analogico": [${diafragma},${distanciaLente},${distanciaPantalla}]}`
+    const curlPOST = `curl -X POST ${URL_ARDUINO} -H 'Content-Type: text/plain' -d '${POST} ${body}'`
 
-//     const {
-//         idUsuario,
-//         distanciaLente,
-//         distanciaPantalla,
-//         diafragma
-//     } = req.body
+    return new Promise((resolve, reject) => {
+        exec(curlPOST, (error, stdout, stderr) => {
+            if (error) {
+                reject(error)
+                return
+            }
+            resolve(stdout)
+        })
+    })
+}
 
-//     if (distanciaLente < 0 || distanciaLente > 900) {
-//         res.status(400)
-//             .send("La distancia entre el lente y el foco es menor a 0 o mayor a 900")
-//     } else if (distanciaPantalla < 0 || distanciaPantalla > 900) {
-//         res.status(400)
-//             .send("La distancia entre el lente y la pantalla es menor a 0 o mayor a 900")
-//     } else if (diafragma != "sin diafragma" && diafragma != "diafragma central" && diafragma != "diafragma periferico" && diafragma != "filtro rojo") {
-//         res.status(400)
-//             .send("Diafragma inválido")
-//     } else {
-//         let Diafragma = 0
-//         switch (diafragma) {
-//             case "diafragma central":
-//                 Diafragma = 1
-//                 break
-//             case "diafragma periferico":
-//                 Diafragma = 2
-//                 break
-//             case "filtro rojo":
-//                 Diafragma = 3
-//                 break
-//             default:
-//                 break
-//         }
-        
-//         const body = {
-//             "Estado": [3, true, true],
-//             "Analogico": [Diafragma, distanciaLente, distanciaPantalla]
-//         }
-
-//         let respuestaGet
-//         let message = ''
-
-//         try {
-//             const respuestaPost = axios.post(`${URL_ARDUINO}/1`, body)
-//             let i = 0
-//             do {
-//                 respuestaGet = await axios.get(`${URL_ARDUINO}/${i}`)
-//                 await delay(3000)
-//                 i = i + 1
-//             } while (respuestaGet.data.Estado[2])
-//             switch (respuestaGet.data.Error) {
-//                 case 0:
-//                     message = "laboratorio ok"
-//                     break
-//                 case 1:
-//                     message = "Error en el angulo limite de azimut"
-//                     break
-//                 case 2:
-//                     message = "Error en el angulo limite de elevacion"
-//                     break
-//                 default:
-//                     message = "Error de laboratorio incorrecto"
-//                     break
-//             }
-//             res.status(200).json(message)
-//         } catch (error) {
-//             console.error("-> ERROR postEnsayoConvergente:", error)
-//         }
-//     }
-// }
-
-// async function postEnsayoDivergenteTEST (req, res) {
-//     const {
-//         idUsuario,
-//         distanciaLente,
-//         distanciaLenteLente,
-//         distanciaPantalla,
-//         diafragma
-//     } = req.body
-
-//     if (distanciaLente < 0 || distanciaLente > 700) {
-//         res.status(400).json("la distancia entre el lente y el foco es menor a 0 o mayor a 700")
-//     } else if (distanciaLenteLente < 0 || distanciaLenteLente > 700) {
-//         res.status(400).json("la distancia entre el lente y lente es menor a 0 o mayor a 700")
-//     } else if (distanciaPantalla < 0 || distanciaPantalla > 900) {
-//         res.status(400).json("la distancia entre el lente y la pantalla es menor a 0 o mayor a 900")
-//     } else if (diafragma != "sin diafragma" && diafragma != "diafragma central" && diafragma != "diafragma periferico" && diafragma != "filtro rojo") {
-//         res.status(400).json("el diafragma no es valido")
-//     } else {
-//         let Diafragma = 0
-//         switch (diafragma) {
-//             case "diafragma central":
-//                 Diafragma = 1
-//                 break
-//             case "diafragma periferico":
-//                 Diafragma = 2
-//                 break
-//             case "filtro rojo":
-//                 Diafragma = 3
-//                 break
-//             default:
-//                 break
-//         }
-//         const url = 'http://192.168.100.75:3031/api/control/arduino'//cambiar por ip arduino
-//         const body = {
-//             "Estado": [3, false, true],
-//             "Analogico": [Diafragma, distanciaLente, distanciaPantalla]
-//         }
-//         let respuestaGet
-//         let Msj = ''
-//         try {
-//             const respuestaPost = axios.post(`${url}/1`, body)
-//             let i = 0
-//             do {
-//                 respuestaGet = await axios.get(`${url}/${i}`)
-//                 await delay(3000)
-//                 i = i + 1
-//             } while (respuestaGet.data.Estado[2])
-//             switch (respuestaGet.data.Error) {
-//                 case 0:
-
-//                     Msj = "laboratorio ok"
-//                     break
-//                 case 1:
-//                     Msj = "Error en el angulo limite de azimut"
-//                     break
-//                 case 2:
-//                     Msj = "Error en el angulo limite de elevacion"
-//                     break
-//                 default:
-//                     Msj = "Error de laboratorio incorrecto"
-//                     break
-//             }
-//             res.status(200).json(Msj)
-//         } catch (error) {
-//             console.error("-> ERROR postEnsayoDivergente:", error)
-//         }
-//     }
-// }
